b94ae6395cc3c8477c21b72ec425e32b8c6cbe54
ali.rabiee <a.rabiee@fidilio.com>
2022-08-24 00:47:38 +0430

    added redirect file

--- client/src/pages/ContactUs.jsx
+++ client/src/pages/ContactUs.jsx
@@ -1,36 +1,38 @@
 import Layout from "../layout/Layout";
-
+import { IoLogoLinkedin } from "react-icons/io5";
+import { AiFillInstagram } from "react-icons/ai";
+import { BsTelegram } from "react-icons/bs";
 const ContactUs = () => {
   return (
     <Layout>
-    <div className="container mx-auto px-4">
-      <div className="flex justify-center  space-x-10">
-        <a
-          className=""
-          href="https://www.linkedin.com/company/apsignals/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <i className="fa fa-linkedin text-6xl"></i>
-        </a>
-        <a
-          className=""
-          href="https://t.me/APSignalsAdmin"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <i className="fa fa-telegram text-6xl"></i>
-        </a>
-        <a
-          className=""
-          href="https://www.instagram.com/apsignal/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <i className="fa fa-instagram text-6xl"></i>
-        </a>
+      <div className="container mx-auto px-4">
+        <div className="flex justify-center  space-x-10">
+          <a
+            className=""
+            href="https://www.linkedin.com/company/apsignals/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <IoLogoLinkedin className="text-6xl" />
+          </a>
+          <a
+            className=""
+            href="https://t.me/APSignalsAdmin"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BsTelegram className="text-6xl" />
+          </a>
+          <a
+            className=""
+            href="https://www.instagram.com/apsignal/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <AiFillInstagram className="text-6xl" />
+          </a>
+        </div>
       </div>
-    </div>
     </Layout>
   );
 };
